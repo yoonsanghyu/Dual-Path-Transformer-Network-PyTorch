52c00760f8c5db28c166406ef2b948bfc9b668a4
Sanghyu yoon <51259168+yoonsanghyu@users.noreply.github.com>
2020-12-11 16:18:11 +0900

    Add files via upload

--- separate.py
+++ separate.py
@@ -2,24 +2,41 @@
 
 # Created on 2018/12
 # Author: Kaituo XU
-
+from collections import OrderedDict
 import argparse
 import os
 
 import librosa
+import soundfile as sf
 import torch
 
 from data import EvalDataLoader, EvalDataset
-from conv_tasnet import ConvTasNet
+from dptnet import DPTNet
 from utils import remove_pad
 
 
-parser = argparse.ArgumentParser('Separate speech using Conv-TasNet')
-parser.add_argument('--model_path', type=str, required=True,
+parser = argparse.ArgumentParser('Separate speech using DPTNet')
+
+# Network architecture
+parser.add_argument('--N', default=64, type=int,
+                    help='Number of filters in autoencoder')
+parser.add_argument('--C', default=2, type=int, 
+                    help='Maximum number of speakers')
+parser.add_argument('--L', default=4, type=int, 
+                    help='Length of window in autoencoder') # L=2 in paper
+parser.add_argument('--H', default=4, type=int, 
+                    help='Number of head in Multi-head attention')
+parser.add_argument('--K', default=250, type=int, 
+                    help='segment size')
+parser.add_argument('--B', default=6, type=int, 
+                    help='Number of repeats')
+
+
+parser.add_argument('--model_path', type=str, default='exp/temp/temp_best.pth.tar',
                     help='Path to model file created by training')
 parser.add_argument('--mix_dir', type=str, default=None,
                     help='Directory including mixture wav files')
-parser.add_argument('--mix_json', type=str, default=None,
+parser.add_argument('--mix_json', type=str, default='data/tt/mix.json',
                     help='Json file including mixture wav files')
 parser.add_argument('--out_dir', type=str, default='exp/result',
                     help='Directory putting separated wav files')
@@ -37,8 +54,23 @@ def separate(args):
               "mix_json is ignored.")
 
     # Load model
-    model = ConvTasNet.load_model(args.model_path)
-    print(model)
+    model = DPTNet(args.N, args.C, args.L, args.H, args.K, args.B)
+    
+    if args.use_cuda:
+        model = torch.nn.DataParallel(model)
+        model.cuda()
+    
+    # model.load_state_dict(torch.load(args.model_path, map_location='cpu'))
+    
+    model_info = torch.load(args.model_path)
+
+    state_dict = OrderedDict()
+    for k, v in model_info['model_state_dict'].items():
+        name = k.replace("module.", "")    # remove 'module.'
+        state_dict[name] = v
+    model.load_state_dict(state_dict)
+    
+    
     model.eval()
     if args.use_cuda:
         model.cuda()
@@ -51,7 +83,8 @@ def separate(args):
     os.makedirs(args.out_dir, exist_ok=True)
 
     def write(inputs, filename, sr=args.sample_rate):
-        librosa.output.write_wav(filename, inputs, sr)# norm=True)
+        sf.write(filename, inputs, sr)
+        #librosa.output.write_wav(filename, inputs, sr)# norm=True)
 
     with torch.no_grad():
         for (i, data) in enumerate(eval_loader):
